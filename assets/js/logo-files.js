137dae33511a32759fd99d2b482f510a62b26442
ariefz <ariefzzz2580@gmail.com>
2026-08-18 23:02:15 +0700

    Update logo-files.js

--- assets/js/logo-files.js
+++ assets/js/logo-files.js
@@ -1,1 +1,14 @@
+window.LOGO_FILES = {
+  BBCA: 'assets/logos/bbca.png',
+  BBRI: 'assets/logos/bbri.png',
+  BMRI: 'assets/logos/bmri.png',
+  TLKM: 'assets/logos/tlkm.png',
 
+  AAPL: 'assets/logos/aapl.png',
+  MSFT: 'assets/logos/msft.png',
+  NVDA: 'assets/logos/nvda.png',
+
+  BTC: 'assets/logos/btc.png',
+  ETH: 'assets/logos/eth.png',
+  SOL: 'assets/logos/sol.png',
+};
